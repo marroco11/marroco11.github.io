e256365150b17a9faf92f4a593518f1e8ba2cb3f
marroco11 <157555802+marroco11@users.noreply.github.com>
2024-03-23 21:56:59 -0400

    Working on the first 3 steps on MDN  to complete the result() function

--- Assignment4_Part1/main.js
+++ Assignment4_Part1/main.js
@@ -10,5 +10,35 @@ function randomValueFromArray(array){
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 ;
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insterY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into ", "a puddle on the sidewalkturned into a slug and crawled away"];
+
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray (insertX);
+    const yItem = randomValueFromArray (insertY) ;
+    const zItem = randomValueFromArray (insertZ);
+
+    newStory = newStory.replaceAll(":insertx:", xItem)
+                       .replace(":inserty:", yItem)
+                       .replace(":insertz:", zItem);
+    
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
