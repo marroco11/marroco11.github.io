33dc52e87fd1a0e0fa04716c5d230af69295740c
marroco11 <157555802+marroco11@users.noreply.github.com>
2024-03-24 01:38:09 -0400

    Declaring the array of image filenames

--- Assignment4_Part2/main.js
+++ Assignment4_Part2/main.js
@@ -6,6 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const alts = {
+    'pic1.jpg' : 'Closeup eye',
+    'pic2.jpg' : 'Wavy Texture',
+    'pic3.jpg' : 'Purple White Flowers',
+    'pic4.jpg' : 'Egyptian Wall',
+    'pic5.jpg' : 'Brown Moth'
+}
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
