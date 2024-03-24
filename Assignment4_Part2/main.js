46462bbeed7f21e1b3bb6569b981eecdf3e28c74
marroco11 <157555802+marroco11@users.noreply.github.com>
2024-03-24 02:28:20 -0400

    Looping through images

--- Assignment4_Part2/main.js
+++ Assignment4_Part2/main.js
@@ -7,6 +7,10 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+
+/* Declaring the alternative text for each image file */
+
 const alts = {
     'pic1.jpg' : 'Closeup eye',
     'pic2.jpg' : 'Wavy Texture',
@@ -15,13 +19,17 @@ const alts = {
     'pic5.jpg' : 'Brown Moth'
 }
 
-/* Declaring the alternative text for each image file */
-
 /* Looping through images */
 
+for (const image of images) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', images);
+newImage.setAttribute('alt', alts[images]);
 thumbBar.appendChild(newImage);
+newImage.addEventListener('click', () => {
+    displayedImage.setAttribute('src', newImage.getAttribute('src'));
+    displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
+   })
 
+}
 /* Wiring up the Darken/Lighten button */
