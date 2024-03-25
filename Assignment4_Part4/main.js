3de44bbe68d98fbddd27b28e2ea3f91b9017b23e
marroco11 <157555802+marroco11@users.noreply.github.com>
2024-03-24 21:53:40 -0400

    Checking the results after the modification

--- Assignment4_Part4/main.js
+++ Assignment4_Part4/main.js
@@ -78,19 +78,19 @@ class Ball extends Shape{
 
 // The Collision Detection function
 
-      collisionDetect() {
+    collisionDetect() {
         for (const ball of balls) {
-          if (this !== ball) {
+        if (!(this === ball) && ball.exists) {
             const dx = this.x - ball.x;
             const dy = this.y - ball.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
-      
+    
             if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
+            ball.color = this.color = randomRGB();
             }
-          }
         }
-      }
+        }
+    }
       
       
   }
