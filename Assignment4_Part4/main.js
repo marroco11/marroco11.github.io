7f2720223627700974b3ea4d1eac326fab305b53
marroco11 <157555802+marroco11@users.noreply.github.com>
2024-03-24 21:49:09 -0400

    Working on the Ball class

---
+++ Assignment4_Part4/main.js
@@ -0,0 +1,138 @@
+// setup canvas
+
+// The 4 provided constants
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+// The Shape class with the required constructors
+
+class Shape {
+    constructor(x, y, velX, velY) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;   
+    }
+}
+
+// The Ball class with the constructor
+// Deriving from the Shape class
+
+class Ball extends Shape{
+    constructor(x, y, velX, velY, color, size) {
+     super (x, y, velX, velY); // The super constructor
+
+      this.color = color;
+      this.size = size;
+      this.exists = true; // To track whether the balls exists in the program
+
+    }
+    
+// The draw function
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+
+// The update function
+
+      update() {
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+      
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+      
+        this.x += this.velX;
+        this.y += this.velY;
+      }
+
+// The Collision Detection function
+
+      collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
+      
+      
+  }
+
+  const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+  testBall.draw();
+
+  const balls = [];
+
+  // The whole loop that creates 25 random balls
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+// The loop function
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect(); // Calling the collision detection function
+
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
+
